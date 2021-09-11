c51a772c5016831b5f297ffb247ab110cb1a6b7c
ofek <tikotzkyofek@gmail.com>
2021-09-11 14:38:02 +0300

    on debug - printing jacobi output when goal==SPK

--- simple_tester.sh
+++ simple_tester.sh
@@ -8,22 +8,22 @@ rm tests/our_jacobi_output.csv
 rm tests/our_spk_c_output.csv
 
 echo "${blue}\nWAM TEST RESULTS:"
-./spkmeans 0 wam tests/input.csv > tests/our_wam_output.csv
+./spkmeans 0 wam tests/accurate_rami_inputs > tests/our_wam_output.csv
 echo "${red}"
 diff tests/rami_wam_output.csv tests/our_wam_output.csv
 
 echo "${blue}\nDDG TEST RESULTS:"
-./spkmeans 0 ddg tests/input.csv > tests/our_ddg_output.csv
+./spkmeans 0 ddg tests/accurate_rami_inputs > tests/our_ddg_output.csv
 echo "${red}"
 diff tests/rami_ddg_output.csv tests/our_ddg_output.csv
 
 echo "${blue}\nLNORM TEST RESULTS:"
-./spkmeans 0 lnorm tests/input.csv > tests/our_lnorm_output.csv
+./spkmeans 0 lnorm tests/accurate_rami_inputs > tests/our_lnorm_output.csv
 echo "${red}"
 diff tests/rami_lnorm_output.csv tests/our_lnorm_output.csv
 
 echo "${blue}\nSPK TEST RESULTS:"
-./spkmeans 0 spk tests/input.csv > tests/our_spk_c_output.csv
+./spkmeans 0 spk tests/accurate_rami_inputs > tests/our_spk_c_output.csv
 echo "${red}"
 diff tests/rami_spk_c_output.csv tests/our_spk_c_output.csv
 
